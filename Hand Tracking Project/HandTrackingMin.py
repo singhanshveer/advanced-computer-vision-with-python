89ee5585e2e76916126d08782676d6b0421a6186
Anshveer Singh <notanshveer@gmail.com>
2022-01-06 18:30:53 +0530

    Add images and HAND_CONNECTIONS and fps

--- Hand Tracking Project/HandTrackingMin.py
+++ Hand Tracking Project/HandTrackingMin.py
@@ -4,14 +4,30 @@ import time
 
 cap = cv2.VideoCapture(0)
 
-
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
-    print(results.multi_hand_landmarks)
-
+    # print(results.multi_hand_landmarks)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            # To get to know more about the id's, view the mediapipe-hand-ids in the images directory
+            for id, lm in enumerate(handLms.landmark):
+                # print(id,lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                # Returns the hand id with its 'X' and 'Y' co-ordinates
+                print(id, cx, cy)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 3, (255, 0, 255), 3)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
